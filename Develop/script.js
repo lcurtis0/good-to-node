9134091428605cb1fb096dc3bd12b266f20b969b
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-07-30 10:15:15 -0400

    added some questiosn

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,22 @@
 // TODO: Include packages needed for this application
 
-function writeToFile(fileName, data) {
+const package = require('./package.json');
+const inquirer = require('inquirer');
+const fs = require("fs/promises");
+const { writeFile } = require('fs');
+
+function writeToFile(answers) {
 
     return `
-    ## Turtle-Necklace (Challenge-2)
+    ## ${answers.name}
 
 # Description
 
 This files are created to complete the Module two challenge-2
 
-I want to have a portfolio so that I'm seen more favorable by employeers.
+${answers.description}
 
-Potental employeers should be able to see my latest worka nd description about me.
-
-I learned that flex boxes and are not a simple skill to master. it takes carful consideration of flow and boxes but can be solved by web queries.
+${answers.learned}
 
 Table of Contents
 - [Description](#Decription)
@@ -23,15 +26,11 @@ Table of Contents
 - [License](#License)
 - [Deployment](#Deployment)
 
-N/A
-
 # Usage
-In this asignment is to create a portfolio site for future employeers and uddated later on. For web users they click on the site link and see the site itself. 
+${answers.accomplish}
 
-![alttext](./assets/images/Screenshots/Screenshot%20(110).png)
-![alttext](./assets/images/Screenshots/Screenshot%20(111).png)
-![alttext](./assets/images/Screenshots/Screenshot%20(112).png)
-![alttext](./assets/images/Screenshots/Screenshot%20(113).png)
+![alttext](.${answers.assets} ${answers.images} ${answers.imageName}) 
+
 
 # Credits
 
@@ -63,42 +62,84 @@ N/A
 
 }
 
-const package = require('./package.json');
-const inquirer = require('inquirer');
-//const fs = require(FileSystem);
-
 console.log("Hello and welcome to the README Generator!");
 console.log("Let's get started...");
 
 // TODO: Create an array of questions for user input
 inquirer
-.prompt([
-   { /* Pass your questions in here */
-    type:'input',
-    message: 'What would you like your project title to be?',
-    name:'README Title'
-    },
-    { 
-    type:'input',
-    message: 'What would you like your project title to be?',
-    name:'README Title'
-    }
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    console.log("Here is what you wrote");
-    console.log(answers);
+    .prompt([
+        { /* Pass your questions in here */
+            type: 'input',
+            message: 'What would you like your project title to be?',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'What would you like your description to be?',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'What is something you learned throughout this process?',
+            name: 'learned'
+        },
+        {
+            type: 'input',
+            message: 'Now that you created this application, how do you hope the users will use it?',
+            name: 'accomplish'
+        },
+        {
+            type: 'confirm',
+            message: 'Images are necessary for completing a README file. Do you have an assets folder?',
+            name: 'assets',
+            default: false,
+            transformer: (answer) => (answer ? '/assets' : '')
+        },
+        {
+            type: 'confirm',
+            message: 'If you have a images folder?',
+            name: 'images',
+            default: false,
+            transformer: (answer) => (answer ? '/images' : '')
+        },
+        {
+            type: 'input',
+            message: 'what is the name of your image? [Note: Please do not include file type i.e. imageofRover.png]',
+            name: 'imageName'
+        },
+        {
+            type: 'list',
+            message: 'what type of file is your image?',
+            choice: ['.jpg', '.jpeg', '.png', '.TIFF'],
+            filter(val) {
+                return val.toLowerCase();
+                name: 'fileType'
+            },
+        }
+    ])
 
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-      console.log("error");
-    } else {
-      // Something else went wrong
-      console.log("another type of error");
-    }
-  });
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+        console.log("Here is what you wrote");
+        console.log(answers);
+        writeToFile(answers);
+
+        const writeFile = writeToFile();
+
+        fs.writeFile("README.md", writeFile)
+            .then(() => console.log("Your file has been created!"))
+            .catch((err) => console.error("There was an error: " + err))
+
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            console.log("error");
+        } else {
+            // Something else went wrong
+            console.log("another type of error");
+        }
+    });
 
 const questions = [];
 
@@ -124,7 +165,7 @@ function writeToFile('Newile.txt', data) {}
 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
